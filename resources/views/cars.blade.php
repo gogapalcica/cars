72148c8bf05bc64859d8a3473d68e0c6a36d1d99
gogapalcica <gogapalackovic@gmail.com>
2022-11-24 21:44:03 +0100

    commit 6

--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -1,30 +1,16 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Welcome to Cars Project</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body class="antialiased">
-        <h2>automobili</h2>
+@extends('layouts.master')
+        
+@section('title', 'cars')      
+        
+@section('content')      
+    <h2>automobili</h2>
 
 
-        <ul>
+    <ul>
         @foreach ($cars as $car)
             <li><a href= "{{route('OnlyCar',['id'=> $car->id])}}">{{$car->title}}</a></li>
 
         @endforeach
-        </ul>
+    </ul>
         
-    </body>
-</html>
+@endsection
